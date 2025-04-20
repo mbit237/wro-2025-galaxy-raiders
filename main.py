161abe0d54bfcfeae5d5cf1f2a7639ba5a45689c
mbit237 <mbit237@gmail.com>
2025-04-20 19:01:53 +0800

    added compass

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import pigpio
 import time 
 import struct
 import smbus
+import math 
 
 MM_PER_STEPS = 0.298 
 
@@ -89,18 +90,73 @@ class Gyro:
         self.prev_z = z
         self.prev_time = now
 
+class Compass:
+    SCALE_880G = 0b000
+    SCALE_1300G = 0b001
+    SCALE_1900G = 0b010
+    SCALE_2500G = 0b011
+    SCALE_4000G = 0b100
+    SCALE_4700G = 0b101
+    SCALE_5600G = 0b110
+    SCALE_8100G = 0b111
+
+    def __init__(self, addr=30, scale=SCALE_880G):
+        self.addr = addr 
+        self.scale = scale 
+        self.init_device()
+    
+    def init_device(self):
+        self.bus = smbus.SMBus(1)
+        self.bus.write_byte_data(self.addr, 0x00, 0b01111000)
+        self.bus.write_byte_data(self.addr, 0x01, self.scale << 5) # << shift the bits to the left by 5
+        # continuous mode, normal speed I2C
+        self.bus.write_byte_data(self.addr, 0x02, 0b00000000)
+
+    def read(self):
+        data = self.bus.read_i2c_block_data(self.addr, 0x03, 6) # start from 03 then go to the next 6 bytes 
+        return struct.unpack('>hhh', bytes(data)) # big endian, small h means it is positive or negative 
+    
+    def heading(self):
+        x, z, y = self.read()
+        sx = (x + 416)*(2/805) - 1 
+        sy = (y + 445)*(2/965) - 1
+        
+        rad = math.atan2(sy, sx)
+        
+        deg = rad * 180 / math.pi 
+        return deg
+
+
+    
+
 # ------------------------Main-------------------------- #
 
 cb1 = pi.callback(5, pigpio.RISING_EDGE, step_count)
 cb2 = pi.callback(6, pigpio.EITHER_EDGE, drive_dir)
 
-gyro = Gyro() #initialise class
+# gyro = Gyro() #initialise class
+compass = Compass()
 print_time = time.time() + 0.5
+lx, ly = 389, 520
+mx, my = -416, -445
 while True:
-    gyro.update_angle()
-    if time.time() > print_time:
-        print(gyro.angle_z())
-        print_time = time.time() + 0.5 
+    print(compass.heading())
+    ## Finding min 
+    # if d[0] > lx:
+    #     lx = d[0]
+    # elif d[0] < mx:
+    #     mx = d[0]
+    # if d[2] > ly:
+    #     ly = d[2]
+    # elif d[2] < my:
+    #     my = d[2]
+    # print(lx, ly)
+    # print(mx, my)
+    # gyro.update_angle()
+    # if time.time() > print_time:
+    #     print(gyro.angle_z())
+    #     print_time = time.time() + 0.5 
+    time.sleep(0.1)
 
     
 
