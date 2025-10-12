e8680c4c1298967b29d9bb21bde6ad46b38467ce
mbit237 <mbit237@gmail.com>
2025-10-12 11:45:29 +0800

    refactoring (untested)

---
+++ initialisation.py
@@ -0,0 +1,165 @@
+import math
+
+def full_path_from_one_section(outer_one_section, inner_one_section):
+    obstacle_inner_paths = inner_one_section
+    obstacle_outer_paths = outer_one_section
+    for a in range(5):
+        p = inner_one_section[a]
+        obstacle_inner_paths.append([[p[0][1], 3000-p[0][0]], [p[1][1], 3000-p[1][0]]])
+        p = outer_one_section[a]
+        obstacle_outer_paths.append([[p[0][1], 3000-p[0][0]], [p[1][1], 3000-p[1][0]]])
+    for a in range(5):
+        p = inner_one_section[a]
+        obstacle_inner_paths.append([[3000-p[0][0], 3000-p[0][1]], [3000-p[1][0], 3000-p[1][1]]])
+        p = outer_one_section[a]
+        obstacle_outer_paths.append([[3000-p[0][0], 3000-p[0][1]], [3000-p[1][0], 3000-p[1][1]]])
+    for a in range(5):
+        p = inner_one_section[a]
+        obstacle_inner_paths.append([[3000-p[0][1], p[0][0]], [3000-p[1][1], p[1][0]]])
+        p = outer_one_section[a]
+        obstacle_outer_paths.append([[3000-p[0][1], p[0][0]], [3000-p[1][1], p[1][0]]])
+
+    print(obstacle_outer_paths, len(obstacle_outer_paths))
+    obstacle_outer_paths[0] = [[400, 1000], [400, 1200]]
+    obstacle_outer_paths[1] = [[400, 1200], [400, 1350]]
+    obstacle_outer_paths[19] = [[600, 700], [559, 700]]
+    return obstacle_outer_paths, obstacle_inner_paths
+    
+def ccw_paths_from_cw(obstacle_outer_paths, obstacle_inner_paths):
+    ccw_obstacle_inner_paths = []
+    ccw_obstacle_outer_paths = []
+    for a in range(len(obstacle_inner_paths)):
+        p = obstacle_inner_paths[a]
+        ccw_obstacle_inner_paths.append([[3000-p[0][0], p[0][1]], [3000-p[1][0], p[1][1]]])
+        p = obstacle_outer_paths[a]
+        ccw_obstacle_outer_paths.append([[3000-p[0][0], p[0][1]], [3000-p[1][0], p[1][1]]])
+
+    ccw_obstacle_outer_paths[1] = [[2600, 1200], [2600, 1350]]
+    ccw_obstacle_outer_paths[2] = [[2600, 1500], [2600, 2000]]
+    return ccw_obstacle_outer_paths, ccw_obstacle_inner_paths
+
+def get_distance(ldr, dir):
+    while True: 
+        if ldr.update():
+            lidar_measurements = ldr.get_measurements()
+            for m in lidar_measurements:
+                if dir+1 > m[0] > dir-1:
+                    return m[1]
+
+def identify_closer_spikes(measurements):
+    closer_spikes = []
+    for d in range(len(measurements)):
+        prev_d = measurements[d-1][1]
+        curr_d = measurements[d][1]
+        next_d = measurements[(d+1) % len(measurements)][1] 
+        #check if the difference between the previous and the current is signifcantly smaller/bigger than the difference between current and next
+        if (prev_d - curr_d > 300 or next_d - curr_d > 300) and (curr_d > 200):
+                closer_spikes.append(measurements[d])
+    
+    return closer_spikes
+
+def initial_pose(ldr):
+    # forward + backward dist, left + right dist, gyro_z
+    fwd_dist = get_distance(0, ldr)
+    left_dist = get_distance(90, ldr)
+    rear_dist = get_distance(180, ldr)
+    right_dist = get_distance(270, ldr)
+
+    #robot is on left side
+    vote = 0
+    while True:
+        if ldr.update():
+            identified_spikes = identify_closer_spikes(ldr.get_measurements())
+            print('spikes: ', identified_spikes)
+            for closer_spike in identified_spikes:
+                if 0 < closer_spike[0] < 180:
+                    vote += 1
+                if 180 < closer_spike[0] < 360:
+                    vote -= 1
+            print('votes:', vote)
+            if vote <= -10: # left side
+                x = (left_dist)
+                if fwd_dist + rear_dist > 3500:
+                    y = 3000 - fwd_dist
+                else:
+                    y = ((3000 - fwd_dist) + rear_dist) / 2
+                print("left side", "x:", x, "y:", y, "fwd_dist:", fwd_dist, "rear_dist:", rear_dist)
+                return [x, y, 90]
+            if vote >= 10: # right side
+                x = ((1000 - right_dist)) + 2000
+                if fwd_dist + rear_dist > 3500:
+                    y = 3000 - fwd_dist
+                else:
+                    y = ((3000 - fwd_dist) + rear_dist) / 2
+                print("right side", "x:", x, "y:", y, "fwd_dist:", fwd_dist, "rear_dist:", rear_dist)
+                return [x, y, 90]
+
+def calc_position_error(matches):
+    if not matches:  # Handle empty matches list
+        return [0, 0]
+    errors = []
+    for match in matches:
+        errorx = match[0] - match[2]
+        errory = match[1] - match[3]
+        errors.append([errorx, errory])
+        
+    err_x = 0
+    err_y = 0
+    for err in errors:
+        err_x += err[0]
+        err_y += err[1]
+    ave_err_x = err_x / len(errors)
+    ave_err_y = err_y / len(errors)
+    
+    return [ave_err_x, ave_err_y]
+
+def calc_pose(pose, error):
+    new_x = pose[0] - error[0]
+    new_y = pose[1] - error[1]
+    
+    return [new_x, new_y, pose[2]]
+
+def merge_positions(odo_pose, spike_pose, POSITION_FILTER_RATIO):
+    odo_x = odo_pose[0]
+    odo_y = odo_pose[1]
+    spike_x = spike_pose[0]
+    spike_y = spike_pose[1]
+    merged_x = odo_x * (1 - POSITION_FILTER_RATIO) + spike_x * POSITION_FILTER_RATIO
+    merged_y = odo_y * (1 - POSITION_FILTER_RATIO) + spike_y * POSITION_FILTER_RATIO
+    
+    return [merged_x, merged_y, odo_pose[2]]
+
+def calc_angle_error(pose, matches):
+    if not matches:
+        return 0
+    
+    angle_errors = []
+    
+    for match in matches:
+        robot_x = pose[0]
+        robot_y = pose[1]
+        landmark_x = match[2]
+        landmark_y = match[3]
+        dx = landmark_x - robot_x
+        dy = landmark_y - robot_y
+        a_theta = math.atan2(dy, dx) * 180 / math.pi # actual theta
+        m_theta = pose[2] + match[4] # measured_theta
+        angle_err = m_theta - a_theta
+        while angle_err > 180:
+            angle_err -= 360
+        while angle_err < -180:
+            angle_err += 360
+        
+        angle_errors.append(angle_err)
+    # print(angle_errors)
+    ave_angle_err = sum(angle_errors) / len(angle_errors)
+    return ave_angle_err
+
+def calc_heading(pose, error):
+    return [pose[0], pose[1], (pose[2] - error)]
+
+def merge_heading(merged_position_pose, spike_pose, HEADING_FILTER_RATIO):
+    angle_z = merged_position_pose[2]
+    spike_z = spike_pose[2]
+    merged_angle = angle_z * (1 - HEADING_FILTER_RATIO) + spike_z * HEADING_FILTER_RATIO
+    return [merged_position_pose[0], merged_position_pose[1], merged_angle]
--- main_open.py
+++ main_open.py
@@ -3,302 +3,89 @@ import pigpio
 import time 
 import struct
 import math 
-# import client_raspi
-
-# import lds02rr
 import coind4
-# from compass import Compass
 from gyro import Gyro 
 import drive
 import spike
 import navigation
 from estimate_pose import estimate_pose, reset_pose
 import client_raspi as client
+from initialisation import *
 
-MM_PER_STEPS = 0.296
+POSITION_FILTER_RATIO = 0.1 
+HEADING_FILTER_RATIO = 0.01  
 
-# ratios need to be tuned
-POSITION_FILTER_RATIO = 0.1 # 0.1% confidence
-HEADING_FILTER_RATIO = 0.01  # 0.1%, if it is too low, heading error will be larger
-                            # if too high, robot will jump around
-
-# paths = [
-#     [[500, 500], [500, 2000]], 
-#     [[500, 2500], [2000, 2500]], 
-#     [[2500, 2500], [2500, 1000]], 
-#     [[2500, 500], [1000, 500]]
-# ]
-
-cw_paths = [ # clockwise paths for open challenge
-    # [[500, 500], [500, 2000]], 
-    # [[500, 2500], [2000, 2500]], 
-    # [[2500, 2500], [2500, 1000]], 
-    # [[2500, 500], [1000, 500]],
+cw_paths = [ 
     [[300, 300], [300, 2350]],
     [[300, 2700], [2350, 2700]],
     [[2700, 2700], [2700, 650]],
     [[2700, 300], [650, 300]]
 ]
-
-ccw_paths = [ # counter-clockwise paths for open challenge
-    # [[500, 500], [500, 2000]], 
-    # [[500, 2500], [2000, 2500]], 
-    # [[2500, 2500], [2500, 1000]], 
-    # [[2500, 500], [1000, 500]],
+ccw_paths = [ 
     [[2700, 300], [2700, 2350]],
     [[2700, 2700], [650, 2700]],
     [[300, 2700], [300, 650]],
     [[300, 300], [2350, 300]]
 ]
 
-obstacle_outer_paths = [
-[[200, 200], [200, 2750]],
-[[200, 2800], [2750, 2800]],
-[[2800, 2800], [2800, 250]],
-[[2800, 200], [250, 200]]
-]
+def run(gyro, ldr, pi):
+    pose = initial_pose(ldr) 
+    stop_y = pose[1] - 50
+    print("Initial pose:", pose)
+    print("angle_z =", gyro.angle_z())
 
-obstacle_inner_paths = [
-[[800, 800], [800, 2150]],
-[[800, 2200], [2150, 2200]],
-[[2200, 2200], [2200, 850]],
-[[2200, 800], [850, 800]]
-]
+    if pose[0] < 1500:
+        paths = cw_paths
+    else:
+        paths = ccw_paths
+    paths = navigation.augment_paths(paths)
+    index = 0
+    print("Paths augmented")
 
-def get_distance(dir):
-    while True: 
+    path_count = 0
+    print('steps', drive.steps)
+    reset_pose()  
+    while True:
+        pose = estimate_pose(pose, gyro.delta_z(), MM_PER_STEPS) 
         if ldr.update():
             lidar_measurements = ldr.get_measurements()
-            for m in lidar_measurements:
-                if dir+1 > m[0] > dir-1:
-                    return m[1]
+            spikes = spike.identify_spikes(lidar_measurements)
+            # print(spikes)
+            c_spikes = spike.add_cartesian(pose, spikes)
+            # print(pose, c_spikes)
+            matches = spike.match_landmarks(c_spikes)
+            # client.send(matches)
+            print(len(matches))
+            while pose[2] > 180:
+                pose[2] -= 360
+            while pose[2] < -180:
+                pose[2] += 360
+            print(pose, matches)
+            
+            position_error = calc_position_error(matches)
+            spike_pose = calc_pose(pose, position_error)
+            print("Pose after position error:", spike_pose)
+            merged_position_pose = merge_positions(pose, spike_pose, POSITION_FILTER_RATIO)
+            angle_error = calc_angle_error(merged_position_pose, matches)
+            # print(angle_error)
+            spike_heading_pose = calc_heading(merged_position_pose, angle_error, HEADING_FILTER_RATIO)
+            # print(spike_heading_pose)
+            merged_pose = merge_heading(merged_position_pose, spike_heading_pose)
+            print("merged_position_pose:", merged_position_pose[2], "spike_heading_pose:", spike_heading_pose[2])
+            # pose = merged_position_pose
+            pose = merged_pose
 
-def identify_closer_spikes(measurements):
-    closer_spikes = []
-    for d in range(len(measurements)):
-        prev_d = measurements[d-1][1]
-        curr_d = measurements[d][1]
-        next_d = measurements[(d+1) % len(measurements)][1] 
-        #check if the difference between the previous and the current is signifcantly smaller/bigger than the difference between current and next
-        if (prev_d - curr_d > 300 or next_d - curr_d > 300) and (curr_d > 200):
-                closer_spikes.append(measurements[d])
-    
-    return closer_spikes
+        count = navigation.drive_paths(index, paths, pose, 250)
+        if count != index:
+            path_count += 1
+        index = count % 4
+        if path_count == 12:
+            if pose[1] >= stop_y:
+                print("Reached stopping pose")
+                break
 
-def initial_pose():
-    # forward + backward dist, left + right dist, gyro_z
-    fwd_dist = get_distance(0)
-    left_dist = get_distance(90)
-    rear_dist = get_distance(180)
-    right_dist = get_distance(270)
-
-    #robot is on left side
-    vote = 0
-    while True:
-        if ldr.update():
-            identified_spikes = identify_closer_spikes(ldr.get_measurements())
-            print('spikes: ', identified_spikes)
-            for closer_spike in identified_spikes:
-                if 0 < closer_spike[0] < 180:
-                    vote += 1
-                if 180 < closer_spike[0] < 360:
-                    vote -= 1
-            print('votes:', vote)
-            if vote <= -10: # left side
-                x = (left_dist)
-                y = ((3000 - fwd_dist) + rear_dist) / 2
-                print("left side", "x:", x, "y:", y)
-                return [x, y, 90]
-            if vote >= 10: # right side
-                x = ((1000 - right_dist)) + 2000
-                y = ((3000 - fwd_dist) + rear_dist) / 2
-                print("right side", "x:", x, "y:", y)
-                return [x, y, 90]
-    # if get_distance(360-50) > 1000 or get_distance(360-34) > 1200 or get_distance(180+35) > 1500 or get_distance(180+15) > 1000:
-    #     robot_x = (left_dist + (1000 - right_dist)) / 2           
-    #     robot_y = (rear_dist + (3000 - fwd_dist)) / 2
-    #     print("left side")
-    #     return [robot_x, robot_y, gyro.angle_z()]
-    # else: # robot is on right side
-    #     robot_x = ((left_dist + 2000) + (3000 - right_dist)) / 2
-    #     robot_y = (rear_dist + (3000 - fwd_dist)) / 2
-    #     print("right side")
-    #     return [robot_x, robot_y, gyro.angle_z()]
-
-def calc_position_error(matches):
-    if not matches:  # Handle empty matches list
-        return [0, 0]
-    errors = []
-    for match in matches:
-        errorx = match[0] - match[2]
-        errory = match[1] - match[3]
-        errors.append([errorx, errory])
-        
-    err_x = 0
-    err_y = 0
-    for err in errors:
-        err_x += err[0]
-        err_y += err[1]
-    ave_err_x = err_x / len(errors)
-    ave_err_y = err_y / len(errors)
-    
-    return [ave_err_x, ave_err_y]
-
-def calc_pose(pose, error):
-    new_x = pose[0] - error[0]
-    new_y = pose[1] - error[1]
-    
-    return [new_x, new_y, pose[2]]
-
-def merge_positions(odo_pose, spike_pose):
-    odo_x = odo_pose[0]
-    odo_y = odo_pose[1]
-    spike_x = spike_pose[0]
-    spike_y = spike_pose[1]
-    merged_x = odo_x * (1 - POSITION_FILTER_RATIO) + spike_x * POSITION_FILTER_RATIO
-    merged_y = odo_y * (1 - POSITION_FILTER_RATIO) + spike_y * POSITION_FILTER_RATIO
-    
-    return [merged_x, merged_y, odo_pose[2]]
-
-def calc_angle_error(pose, matches):
-    if not matches:
-        return 0
-    
-    angle_errors = []
-    
-    for match in matches:
-        robot_x = pose[0]
-        robot_y = pose[1]
-        landmark_x = match[2]
-        landmark_y = match[3]
-        dx = landmark_x - robot_x
-        dy = landmark_y - robot_y
-        a_theta = math.atan2(dy, dx) * 180 / math.pi # actual theta
-        m_theta = pose[2] + match[4] # measured_theta
-        angle_err = m_theta - a_theta
-        while angle_err > 180:
-            angle_err -= 360
-        while angle_err < -180:
-            angle_err += 360
-        
-        angle_errors.append(angle_err)
-    # print(angle_errors)
-    ave_angle_err = sum(angle_errors) / len(angle_errors)
-    return ave_angle_err
-
-def calc_heading(pose, error):
-    return [pose[0], pose[1], (pose[2] - error)]
-
-def merge_heading(merged_position_pose, spike_pose):
-    angle_z = merged_position_pose[2]
-    spike_z = spike_pose[2]
-    merged_angle = angle_z * (1 - HEADING_FILTER_RATIO) + spike_z * HEADING_FILTER_RATIO
-    return [merged_position_pose[0], merged_position_pose[1], merged_angle]
-
-pi = pigpio.pi()
-pi.set_mode(17, pigpio.INPUT)
-pi.set_pull_up_down(17, pigpio.PUD_UP)
-print('steps', drive.steps)
-# while True:
-#     if ldr.update():
-#         break
-# print('cleared')
-ldr = coind4.CoinD4() #lidar initialise
-ldr.start()
-print("Lidar started")
-# compass = Compass()
-gyro = Gyro() #initialise class
-gyro.calibration()
-print("Gyro calibrated")
-
-# print('start')
-
-# client.connect()
-# print("Client connected")
-
-# while True:
-#     initial_pose()
-#     print('done')
-    # pose = estimate_pose(initial_pose(), gyro.delta_z(), MM_PER_STEPS)
-    # if ldr.update():
-    #     print("working")
-    #     lidar_measurements = ldr.get_measurements()
-    #     closer_spikes = identify_closer_spikes(lidar_measurements)
-    #     print(closer_spikes)
-
-pose = initial_pose()
-stop_y = pose[1] - 50
-print("Initial pose:", pose)
-print("angle_z =", gyro.angle_z())
-
-if pose[0] < 1500:
-    paths = cw_paths
-else:
-    paths = ccw_paths
-paths = navigation.augment_paths(paths)
-index = 0
-print("Paths augmented")
-
-# pose = [500, 500, 90] # Initial pose for testing
-# time.sleep(2)
-# count = 0
-path_count = 0
-print('steps', drive.steps)
-reset_pose()  # Reset the pose to the initial state
-while True:
-    pose = estimate_pose(pose, gyro.delta_z(), MM_PER_STEPS) 
-    # print("Pose:", pose)
-    if ldr.update():
-        # print("working", time.time())
-        lidar_measurements = ldr.get_measurements()
-        # for l in lidar_measurements:
-        #     print(l)
-        # print(len(lidar_measurements))
-        # break
-        #spike detection
-        spikes = spike.identify_spikes(lidar_measurements)
-        # print(spikes)
-        c_spikes = spike.add_cartesian(pose, spikes)
-        # print(pose, c_spikes)
-        matches = spike.match_landmarks(c_spikes)
-        # client.send(matches)
-        print(len(matches))
-        while pose[2] > 180:
-            pose[2] -= 360
-        while pose[2] < -180:
-            pose[2] += 360
-        print(pose, matches)
-        
-        # if len(matches) == 0:
-        #     count += 1
-        #     if count > 5:
-        #         drive.drive(0)
-        #         time.sleep(20)
-        #         count = 0
-        position_error = calc_position_error(matches)
-        spike_pose = calc_pose(pose, position_error)
-        print("Pose after position error:", spike_pose)
-        merged_position_pose = merge_positions(pose, spike_pose)
-        angle_error = calc_angle_error(merged_position_pose, matches)
-        # print(angle_error)
-        spike_heading_pose = calc_heading(merged_position_pose, angle_error)
-        # print(spike_heading_pose)
-        merged_pose = merge_heading(merged_position_pose, spike_heading_pose)
-        print("merged_position_pose:", merged_position_pose[2], "spike_heading_pose:", spike_heading_pose[2])
-        # pose = merged_position_pose
-        pose = merged_pose
-
-    count = navigation.drive_paths(index, paths, pose, 250)
-    if count != index:
-        path_count += 1
-    index = count % 4
-    if path_count == 12:
-        if pose[1] >= stop_y:
-            print("Reached stopping pose")
+        if pi.read(17) == 0:
             break
 
-    if pi.read(17) == 0:
-        break
-
-drive.drive(0)  
-drive.steering(0)  
+    drive.drive(0)  
+    drive.steering(0)  
