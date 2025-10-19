df445ee55765850e6719104198150f266f69358f
mbit237 <mbit237@gmail.com>
2025-10-19 09:14:03 +0800

    edited initialiser sh file

--- initialiser.sh
+++ initialiser.sh
@@ -1,14 +1,5 @@
 #!/bin/sh
 cd /home/joshua
 while true; do
-echo doing
-if python /home/joshua/main.py; then 
-    echo open > /home/joshua/fur.txt
-    python /main_open.py 
-else 
-    echo obstacle > /home/joshua/fur.txt
-    python /main_obstacle.py
-fi
-echo done
-sleep 3
+python /main.py
 done
